dc2240f843c71b341434d0c4866d43dc0168bf79
Pedro Lucas <91135615+PedroLucas3542@users.noreply.github.com>
2023-05-09 20:46:04 -0300

    least css

--- ProjetoWDA/dashboard.php
+++ ProjetoWDA/dashboard.php
@@ -8,58 +8,6 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     exit();
 }
 ?>
-<style>
-    /* Estilo geral */
-    body {
-        font-family: Arial, sans-serif;
-        background-color: #f0f0f0;
-        margin: 0;
-        padding: 0;
-    }
-
-    .container {
-        max-width: 1200px;
-        margin: 0 auto;
-        padding: 20px;
-    }
-
-    /* Estilo do dashboard */
-    .dashboard-container {
-        background-color: #fff;
-        border-radius: 5px;
-        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-        padding: 20px;
-    }
-
-    .dashboard-title {
-        font-size: 24px;
-        margin-bottom: 20px;
-    }
-
-    .dashboard-section {
-        margin-bottom: 30px;
-    }
-
-    .dashboard-section-title {
-        font-size: 20px;
-        margin-bottom: 10px;
-    }
-
-    .dashboard-card {
-        background-color: #f5f5f5;
-        border-radius: 5px;
-        padding: 20px;
-    }
-
-    .dashboard-card-title {
-        font-size: 18px;
-        margin-bottom: 10px;
-    }
-
-    .dashboard-card-value {
-        font-size: 24px;
-    }
-</style>
 
 <link rel="stylesheet" href="css/bootstrap.min.css">
 <nav class="navbar navbar-expand-lg bg-light">
@@ -96,12 +44,12 @@ $resultado_atrasados = mysqli_query($conn, $query_atrasados);
 $livros_atrasados = mysqli_fetch_assoc($resultado_atrasados)['total_atrasados'];
 
 // Quantidade de livros devolvidos dentro do prazo
-$query_devolvidos_prazo = "SELECT COUNT(*) AS total_devolvidos_prazo FROM devolvidos";
+$query_devolvidos_prazo = "SELECT COUNT(*) AS total_devolvidos_prazo FROM devolvidos WHERE prazo = 's'";
 $resultado_devolvidos_prazo = mysqli_query($conn, $query_devolvidos_prazo);
 $livros_devolvidos_prazo = mysqli_fetch_assoc($resultado_devolvidos_prazo)['total_devolvidos_prazo'];
 
 // Quantidade de livros devolvidos fora do prazo
-$query_devolvidos_fora_prazo = "SELECT COUNT(*) AS total_devolvidos_fora_prazo FROM devolvidos";
+$query_devolvidos_fora_prazo = "SELECT COUNT(*) AS total_devolvidos_fora_prazo FROM devolvidos WHERE prazo = 'n'";
 $resultado_devolvidos_fora_prazo = mysqli_query($conn, $query_devolvidos_fora_prazo);
 $livros_devolvidos_fora_prazo = mysqli_fetch_assoc($resultado_devolvidos_fora_prazo)['total_devolvidos_fora_prazo'];
 
@@ -118,30 +66,54 @@ while ($row = mysqli_fetch_assoc($resultado_alugueis_por_usuario)) {
 // Livro mais alugado
 $query_livro_mais_alugado = "SELECT livro_id, COUNT(*) AS total_alugueis FROM emprestimos GROUP BY livro_id ORDER BY total_alugueis DESC LIMIT 1";
 $resultado_livro_mais_alugado = mysqli_query($conn, $query_livro_mais_alugado);
-$livro_mais_alugado_id = mysqli_fetch_assoc($resultado_livro_mais_alugado)['livro_id'];
+@$livro_mais_alugado_id = mysqli_fetch_assoc($resultado_livro_mais_alugado)['livro_id'];
 
 // Recupera o nome do livro mais alugado
 $query_nome_livro_mais_alugado = "SELECT nome FROM livros WHERE id = $livro_mais_alugado_id";
 $resultado_nome_livro_mais_alugado = mysqli_query($conn, $query_nome_livro_mais_alugado);
-$livro_mais_alugado_nome = mysqli_fetch_assoc($resultado_nome_livro_mais_alugado)['nome'];
+@$livro_mais_alugado_nome = mysqli_fetch_assoc($resultado_nome_livro_mais_alugado)['nome'];
 
 // Quantidade de livros devolvidos
-$query_devolvidos = "SELECT COUNT(*) AS total_devolvidos FROM emprestimos";
+$query_devolvidos = "SELECT COUNT(*) AS total_devolvidos FROM devolvidos";
 $resultado_devolvidos = mysqli_query($conn, $query_devolvidos);
 $livros_devolvidos = mysqli_fetch_assoc($resultado_devolvidos)['total_devolvidos'];
 
+
 // Exibir as informações no dashboard
-echo "<div class='dashboard-container'>";
-echo "<center><h2>Dashboard</h2>";
-echo "<p>Quantidade de livros emprestados: $livros_emprestados</p>";
+echo "<br><div class='dashboard-container'>";
+echo "<center><h2>Dashboard</h2><br>";
+if($livros_emprestados > 0){
+    echo "<p>Quantidade de livros emprestados: $livros_emprestados</p>";
+}else{
+    echo "<p>Não há livros emprestados.</p>";
+}
+
+if($livros_atrasados > 0){
 echo "<p>Quantidade de livros atrasados: $livros_atrasados</p>";
+}else{
+    echo "<p>Não há livros atrasados.</p>";
+}
+
+if($livros_devolvidos_prazo > 0){
 echo "<p>Quantidade de livros devolvidos dentro do prazo: $livros_devolvidos_prazo</p>";
+}else{
+    echo "<p>Não há livros devolvidos no prazo</p>";
+}
+
+if($livros_devolvidos_fora_prazo > 0){
 echo "<p>Quantidade de livros devolvidos fora do prazo: $livros_devolvidos_fora_prazo</p>";
-echo "<h3>Quantidade de aluguéis por usuário:</h3>";
+}else{
+    echo "<p>Não há livros devolvidos fora do prazo</p>";
+}
+
+echo "<h3>Quantidade de aluguéis por usuário:</h3><br>";
 foreach ($alugueis_por_usuario as $usuario_id => $total_alugueis) {
     echo "<p>Usuário ID $usuario_id: $total_alugueis aluguéis</p>";
 }
 echo "<p>Livro mais alugado: $livro_mais_alugado_nome</p>";
 echo "<p>Quantidade de livros devolvidos: $livros_devolvidos</p></center>";
-echo "</div>"
+echo "</div>";
+
+
+
 ?>
--- ProjetoWDA/devolver.php
+++ ProjetoWDA/devolver.php
@@ -1,6 +1,8 @@
 <?php
 include_once('conexao.php');
 
+$hoje = date('Y-m-d');
+
 // Recebe o ID do empréstimo a ser devolvido
 $emprestimo_id = $_GET['id'];
 
@@ -10,22 +12,28 @@ $resultado_emprestimo = mysqli_query($conn, $query_emprestimo);
 
 if (mysqli_num_rows($resultado_emprestimo) > 0) {
     $emprestimo = mysqli_fetch_assoc($resultado_emprestimo);
-    
+    $prazo = $emprestimo['prazo_entrega'];
+
     // Recupera o ID do livro do empréstimo
     $livro_id = $emprestimo['livro_id'];
-    
+
     // Atualiza a tabela de livros para incrementar o estoque
     $update_livro_query = "UPDATE livros SET estoque = estoque + 1 WHERE id = $livro_id";
     mysqli_query($conn, $update_livro_query);
 
-    $insert_devolvido_query = "INSERT INTO devolvidos (livro_id) VALUES ('$emprestimo_id')";
+    if ($prazo > $hoje) {
+        $prazo_status = "s";
+    } else {
+        $prazo_status = "n";
+    }
+
+    $insert_devolvido_query = "INSERT INTO devolvidos (livro_id, prazo) VALUES ('$emprestimo_id', '$prazo_status')";
     mysqli_query($conn, $insert_devolvido_query);
-    
+
     // Apaga o registro do empréstimo
     $delete_emprestimo_query = "DELETE FROM emprestimos WHERE id = $emprestimo_id";
     mysqli_query($conn, $delete_emprestimo_query);
 
-    
     echo "Devolução realizada com sucesso!";
 } else {
     echo "Empréstimo não encontrado.";

7baa82234f33de759d388c746c5699ed4bba4f47
Pedro Lucas <91135615+PedroLucas3542@users.noreply.github.com>
2023-05-22 13:35:49 -0300

    status added

--- ProjetoWDA/emprestar.php
+++ ProjetoWDA/emprestar.php
@@ -1,20 +1,3 @@
-<?php
-
-include 'conexao.php';
-
-// Consulta SQL para buscar os livros
-$livros_query = "SELECT id, nome FROM livros";
-$livros_result = mysqli_query($conn, $livros_query);
-
-// Consulta SQL para buscar os usuários
-$usuarios_query = "SELECT id, nome FROM usuarios";
-$usuarios_result = mysqli_query($conn, $usuarios_query);
-
-// Consulta SQL para buscar os prazos
-$prazos_query = "SELECT id, dias FROM prazos";
-$prazos_result = mysqli_query($conn, $prazos_query);
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -22,12 +5,71 @@ $prazos_result = mysqli_query($conn, $prazos_query);
     <title>Empréstimo</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <style>
-        /* Estilos CSS restantes ... */
+        body {
+            background-color: #f8f9fa;
+            font-family: Arial, sans-serif;
+        }
+
+        .loan-container {
+            max-width: 400px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+
+        .loan-container h2 {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        .loan-container label {
+            font-weight: bold;
+        }
+
+        .loan-container select,
+        .loan-container input[type="date"] {
+            width: 100%;
+            padding: 8px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+            margin-bottom: 10px;
+        }
+
+        .loan-container input[type="submit"] {
+            width: 100%;
+            padding: 10px;
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        .loan-container input[type="submit"]:hover {
+            background-color: #0056b3;
+        }
     </style>
 </head>
 <body>
-    <!-- Resto do conteúdo HTML ... -->
+    <?php
+        include 'conexao.php';
 
+            // Consulta SQL para buscar os livros
+        $livros_query = "SELECT id, nome FROM livros";
+        $livros_result = mysqli_query($conn, $livros_query);
+
+        // Consulta SQL para buscar os usuários
+        $usuarios_query = "SELECT id, nome FROM usuarios";
+        $usuarios_result = mysqli_query($conn, $usuarios_query);
+
+        // Consulta SQL para buscar os prazos
+        $prazos_query = "SELECT id, dias FROM prazos";
+        $prazos_result = mysqli_query($conn, $prazos_query);
+        ?> 
+        <br><br><br><br>
     <div class="loan-container">
         <h2>Empréstimo</h2>
         <form id="loan-form" action="realizar_emprestimo.php" method="POST">
@@ -46,7 +88,7 @@ $prazos_result = mysqli_query($conn, $prazos_query);
             </select>
             <br><br>
             <label for="prazo">Prazo:</label>
-			<input type="date" id="prazo" name="prazo" min="<?php echo date('Y-m-d'); ?>" max="<?php echo date('Y-m-d', strtotime('+30 days')); ?>">
+            <input type="date" id="prazo" name="prazo" min="<?php echo date('Y-m-d'); ?>" max="<?php echo date('Y-m-d', strtotime('+30 days')); ?>">
             <br><br>
             <input type="submit" value="Realizar Empréstimo">
         </form>
--- ProjetoWDA/emprestimos.php
+++ ProjetoWDA/emprestimos.php
@@ -12,80 +12,106 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Lista de Empréstimos</title>
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="style.css">
-	<script src="js/bootstrap.bundle.min.js"></script>
+    <title>Lista de Empréstimos</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <style>
+        .table-success {
+            background-color: #c3e6cb;
+        }
+        .table-warning {
+            background-color: #ffeeba;
+        }
+        .table-danger {
+            background-color: #f5c6cb;
+        }
+    </style>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-		<a class="navbar-brand" href="#">
-      		<img src="logo.png" alt="Bootstrap" width="90" height="72">
-    	</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="logo.png" alt="Bootstrap" width="90" height="72">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-              <a class="nav-link btn btn-light" href="usuarios.php">Usuários</a>
-			  <a class="nav-link btn btn-light" href="livros.php">Livros</a>
-			  <a class="nav-link btn btn-light" href="emprestimos.php">Empréstimos</a>
-			  <a class="nav-link btn btn-light" href="editoras.php">Editoras</a>
-			  <a class="nav-link btn btn-light" href="atrasos.php">Atrasos</a>
-			  <a class="nav-link btn btn-light" href="dashboard.php" class="dashboard-button">Dashboard</a>
-			  <a class="nav-link btn btn-light" href="logout.php">Sair</a>
+                <a class="nav-link btn btn-light" href="usuarios.php">Usuários</a>
+                <a class="nav-link btn btn-light" href="livros.php">Livros</a>
+                <a class="nav-link btn btn-light" href="emprestimos.php">Empréstimos</a>
+                <a class="nav-link btn btn-light" href="editoras.php">Editoras</a>
+                <a class="nav-link btn btn-light" href="atrasos.php">Atrasos</a>
+                <a class="nav-link btn btn-light" href="dashboard.php" class="dashboard-button">Dashboard</a>
+                <a class="nav-link btn btn-light" href="logout.php">Sair</a>
             </div>
-          </div>
         </div>
-      </nav>
-	  <br><br>
-	<center><h1>Lista de Empréstimos</h1></center><br><br>
-    <div class="container">
-	<a href="pesquisa.php"><button type="button" class="button-background-move">Pesquisar Empréstimo</button></a> - <a href="emprestar.php"><button type="button" class="button-background-move">Efetuar Empréstimo</button></a><br><br>
-	<table class="table table-danger table-striped">
-		<thead>
-			<tr>
-				<th>ID</th>
-				<th>Id Livro</th>
-                <th>Livro</th>
-				<th>Id Usuário</th>
-                <th>Usuário</th>
-				<th>Prazo</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php
-				include 'conexao.php';
-
-				// Query SQL para selecionar os empréstimos
-				$sql = "SELECT * FROM emprestimos";
-
-				$result = mysqli_query($conn, $sql);
-
-				// Verifica se existem empréstimos cadastrados
-				if (mysqli_num_rows($result) > 0) {
-				    // Loop para imprimir cada empréstimo em uma linha da tabela
-				    while($row = mysqli_fetch_assoc($result)) {
-				        echo "<tr>";
-				        echo "<td>" . $row["id"] . "</td>";
-				        echo "<td>" . $row["livro_id"] . "</td>";
-				        echo "<td>" . $row["livro_nome"] . "</td>";
-				        echo "<td>" . $row["usuario_id"] . "</td>";
-                        echo "<td>" . $row["usuario_nome"] . "</td>";
-				        echo "<td>" . $row["prazo_entrega"] . "</td>";
-                        echo "<td><a href='devolver.php?id=".$row["id"]."'><button type='button' class='btn btn-secondary'>Devolver</button></a></td>";
-				        echo "</tr>";
-				    }
-				} else {
-				    echo "Nenhum empréstimo cadastrado.";
-				}
-
-				// Fecha a conexão com o banco de dados
-				mysqli_close($conn);
-			?>
-		</tbody>
-	</table>
     </div>
+</nav>
+<br><br>
+<center><h1>Lista de Empréstimos</h1></center><br><br>
+<div class="container">
+    <a href="pesquisa.php"><button type="button" class="button-background-move">Pesquisar Empréstimo</button></a> - <a href="emprestar.php"><button type="button" class="button-background-move">Efetuar Empréstimo</button></a><br><br>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Id Livro</th>
+                <th>Livro</th>
+                <th>Id Usuário</th>
+                <th>Usuário</th>
+                <th>Prazo</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                include 'conexao.php';
+
+                // Query SQL para selecionar os empréstimos
+                $sql = "SELECT * FROM emprestimos";
+
+                $result = mysqli_query($conn, $sql);
+
+                // Verifica se existem empréstimos cadastrados
+                if (mysqli_num_rows($result) > 0) {
+                    // Loop para imprimir cada empréstimo em uma linha da tabela
+                    while($row = mysqli_fetch_assoc($result)) {
+                        echo "<tr>";
+                        echo "<td>" . $row["id"] . "</td>";
+                        echo "<td>" . $row["livro_id"] . "</td>";
+                        echo "<td>" . $row["livro_nome"] . "</td>";
+                        echo "<td>" . $row["usuario_id"] . "</td>";
+                        echo "<td>" . $row["usuario_nome"] . "</td>";
+                        echo "<td>" . $row["prazo_entrega"] . "</td>";
+                        
+                        // Verifica o status do empréstimo
+                        $dataAtual = date('Y-m-d');
+                        $prazoEntrega = $row["prazo_entrega"];
+                        $status = "";
+                        
+                        if ($prazoEntrega < $dataAtual) {
+                            $status = "Atrasado";
+                            echo "<td class='table-danger'>$status</td>";
+                        } else {
+                            $status = "Dentro do prazo";
+                            echo "<td class='table-success'>$status</td>";
+                        }
+                        
+                        echo "<td><a href='devolver.php?id=".$row["id"]."'><button type='button' class='btn btn-secondary'>Devolver</button></a></td>";
+                        echo "</tr>";
+                    }
+                } else {
+                    echo "<tr><td colspan='7'>Nenhum empréstimo cadastrado.</td></tr>";
+                }
+
+                // Fecha a conexão com o banco de dados
+                mysqli_close($conn);
+            ?>
+        </tbody>
+    </table>
+</div>
 </body>
 </html>
